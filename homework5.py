0460dc7a4abb6ecfab4863b4f920fb5f3ff6d203
Victor Averin <victor.cybernetic@gmail.com>
2024-07-16 03:23:22 +0300

    homework5.py

--- homework5.py
+++ homework5.py
@@ -5,11 +5,11 @@ print(type(immutable_var))
 # immutable_var[0] = 200
 # print(immutable_var[0])
 
-mutable_list = ([1, 2, 3, 4], True, "Hello world")
+mutable_list = [1, 2, 3, 4, True, "Hello world"]
 print(mutable_list)
 print(type(mutable_list))
-mutable_list[0][0] = 4
-mutable_list[0][1] = 3
-mutable_list[0][2] = 2
-mutable_list[0][3] = 1
+mutable_list[0] = 4
+mutable_list[1] = 3
+mutable_list[2] = 2
+mutable_list[3] = 1
 print(mutable_list)
